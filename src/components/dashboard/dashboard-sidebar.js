383a5e32188361c302d853d18bae7c545d75cbd0
sivaprasadvishnu18 <47301401+sivaprasadvishnu18@users.noreply.github.com>
2022-10-10 23:48:02 -0500

    Added getSectionsAdministrator

--- src/components/dashboard/dashboard-sidebar.js
+++ src/components/dashboard/dashboard-sidebar.js
@@ -9,13 +9,13 @@ import { Home as HomeIcon } from '../../icons/home';
 import { DocumentText as DocumentIcon } from '../../icons/document-text';
 import { Scrollbar } from '../scrollbar';
 import { DashboardSidebarSection } from './dashboard-sidebar-section';
-import {PencilAlt as PenIcon} from '../../icons/pencil-alt'
-import {ViewList as GradesIcon} from '../../icons/view-list';
+import { PencilAlt as PenIcon } from '../../icons/pencil-alt'
+import { ViewList as GradesIcon } from '../../icons/view-list';
 import { Language as LanguageIcon } from '../../icons/language';
 import { Save as SaveIcon } from '../../icons/save';
-import {DotsHorizontal as DotsHorizontalIcon} from '../../icons/dots-horizontal'
+import { DotsHorizontal as DotsHorizontalIcon } from '../../icons/dots-horizontal'
 
-const getSections = (t) => [
+const getSectionsStudent = (t) => [
 	{
 		title: t("General"),
 		items: [
@@ -27,7 +27,7 @@ const getSections = (t) => [
 			{
 				title: t("Homework"),
 				path: "/student/homework_portal",
-				icon: <DocumentIcon fontSize="small"/>,
+				icon: <DocumentIcon fontSize="small" />,
 			},
 			{
 				title: t("Practice"),
@@ -64,189 +64,209 @@ const getSectionsTeacher = (t) => [
 			{
 				title: t("Homework"),
 				path: "/teacher/homework_portal",
-				icon: <DocumentIcon fontSize="small"/>,
+				icon: <DocumentIcon fontSize="small" />,
 			},
 			{
 				title: t("More"),
 				path: "/teacher/word-list",
 				icon: <DotsHorizontalIcon fontSize="small" />,
-				children:[
+				children: [
 					{
 						title: t("Word's Library"),
 						path: '/teacher/wordlist'
-					  },
-					  {
+					},
+					{
 						title: t('Manage Classes'),
 						path: '/teacher/manage_class'
-					  },
-					  {
+					},
+					{
 						title: t('Add Homework'),
 						path: '/teacher/addHomework'
-					  },
+					},
 				]
 			},
 		],
 	}
 ];
-    
-    export const DashboardSidebar = (props) => {
-		const {onClose, open} = props;
-		const {logout,user} = useAuth();
-		const router = useRouter();
-		const {t} = useTranslation();
-		const lgUp = useMediaQuery((theme) => theme.breakpoints.up("lg"), {
-			noSsr: true,
-		});
-		const sections = useMemo(() => user.status==="Student"? getSections(t):getSectionsTeacher(t) , [t]);
-
-		const handleLogout = async () => {
-			try {
-				onClose?.();
-				await logout();
-				router.push("/");
-			} catch (err) {
-				console.error(err);
-				toast.error("Unable to logout.");
+const getSectionsAdministrator = (t) => [
+	{
+		title: t("General"),
+		items: [
+			{
+				title: t("General"),
 			}
-		};
-    
-		const handlePathChange = () => {
-			if (!router.isReady) {
-				return;
-			}
+		]
+	}
 
-			if (open) {
-				onClose?.();
-			}
-		};
+];
 
-		useEffect(
-			handlePathChange,
-			// eslint-disable-next-line react-hooks/exhaustive-deps
-			[router.isReady, router.asPath]
-		);
+export const DashboardSidebar = (props) => {
+	const { onClose, open } = props;
+	const { logout, user } = useAuth();
+	const router = useRouter();
+	const { t } = useTranslation();
+	const lgUp = useMediaQuery((theme) => theme.breakpoints.up("lg"), {
+		noSsr: true,
+	});
+	//const sections = useMemo(() => user.status === "Student" ? getSectionsStudent(t): getSectionsTeacher(t), [t]);
+	const sections = useMemo(() => {
+		if (user.status === "student") {
+			getSectionsStudent(t)
+		} else if (user.staus === "teacher") {
+			getSectionsTeacher(t);
+		} else if (user.status === "administrator") {
+			getSectionsAdministrator(t);
+		}
+	});
 
-		const content = (
-			<>
-				<Scrollbar
-					sx={{
-						height: "100%",
-						"& .simplebar-content": {
-							height: "100%",
-						},
-					}}
-				>
-					<Box
-						sx={{
-							display: "flex",
-							flexDirection: "column",
-							height: "100%",
-						}}
-					>
-						<div>
-							<Box
-								sx={{
-									display: "flex",
-									alignItems: "center",
-									justifyContent: "center",
-								}}
-							>
-								<NextLink href="/" passHref>
-									<img 
-										alt='Seeing is Believing' 
-										src='/static/icons/logo.png' 
-										style={{width: '70%', marginTop: -3, objectFit: 'cover'}}
-									/>
-								</NextLink>
-							</Box>
-						</div>
-						<Divider
-							sx={{
-								borderColor: "#2D3748",
-								my: 1,
-							}}
-						/>
-						<Box sx={{flexGrow: 1}}>
-							{sections.map((section) => (
-								<DashboardSidebarSection
-									key={section.title}
-									path={router.asPath}
-									sx={{
-										mt: 2,
-										"& + &": {
-											mt: 2,
-										},
-									}}
-									{...section}
-								/>
-							))}
-						</Box>
-						<Divider
-							sx={{
-								borderColor: "#2D3748", // dark divider
-							}}
-						/>
-						<Box sx={{p: 2}}>
-								<Button
-									color="primary"
-									component="a"
-									fullWidth
-									sx={{mt: 2}}
-									variant="contained"
-									onClick={handleLogout}
-								>
-									{t("Logout")}
-								</Button>
-						</Box>
-					</Box>
-				</Scrollbar>
-			</>
-		);
+	const handleLogout = async () => {
+		try {
+			onClose?.();
+			await logout();
+			router.push("/");
+		} catch (err) {
+			console.error(err);
+			toast.error("Unable to logout.");
+		}
+	};
 
-		if (lgUp) {
-			return (
-				<Drawer
-					anchor="left"
-					open
-					PaperProps={{
-						sx: {
-							backgroundColor: "neutral.900",
-							borderRightColor: "divider",
-							borderRightStyle: "solid",
-							borderRightWidth: (theme) =>
-								theme.palette.mode === "dark" ? 1 : 0,
-							color: "#FFFFFF",
-							width: 280,
-						},
-					}}
-					variant="permanent"
-				>
-					{content}
-				</Drawer>
-			);
+	const handlePathChange = () => {
+		if (!router.isReady) {
+			return;
 		}
 
+		if (open) {
+			onClose?.();
+		}
+	};
+
+	useEffect(
+		handlePathChange,
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+		[router.isReady, router.asPath]
+	);
+
+	const content = (
+		<>
+			<Scrollbar
+				sx={{
+					height: "100%",
+					"& .simplebar-content": {
+						height: "100%",
+					},
+				}}
+			>
+				<Box
+					sx={{
+						display: "flex",
+						flexDirection: "column",
+						height: "100%",
+					}}
+				>
+					<div>
+						<Box
+							sx={{
+								display: "flex",
+								alignItems: "center",
+								justifyContent: "center",
+							}}
+						>
+							<NextLink href="/" passHref>
+								<img
+									alt='Seeing is Believing'
+									src='/static/icons/logo.png'
+									style={{ width: '70%', marginTop: -3, objectFit: 'cover' }}
+								/>
+							</NextLink>
+						</Box>
+					</div>
+					<Divider
+						sx={{
+							borderColor: "#2D3748",
+							my: 1,
+						}}
+					/>
+					<Box sx={{ flexGrow: 1 }}>
+						{sections.map((section) => (
+							<DashboardSidebarSection
+								key={section.title}
+								path={router.asPath}
+								sx={{
+									mt: 2,
+									"& + &": {
+										mt: 2,
+									},
+								}}
+								{...section}
+							/>
+						))}
+					</Box>
+					<Divider
+						sx={{
+							borderColor: "#2D3748", // dark divider
+						}}
+					/>
+					<Box sx={{ p: 2 }}>
+						<Button
+							color="primary"
+							component="a"
+							fullWidth
+							sx={{ mt: 2 }}
+							variant="contained"
+							onClick={handleLogout}
+						>
+							{t("Logout")}
+						</Button>
+					</Box>
+				</Box>
+			</Scrollbar>
+		</>
+	);
+
+	if (lgUp) {
 		return (
 			<Drawer
 				anchor="left"
-				onClose={onClose}
-				open={open}
+				open
 				PaperProps={{
 					sx: {
 						backgroundColor: "neutral.900",
+						borderRightColor: "divider",
+						borderRightStyle: "solid",
+						borderRightWidth: (theme) =>
+							theme.palette.mode === "dark" ? 1 : 0,
 						color: "#FFFFFF",
 						width: 280,
 					},
 				}}
-				sx={{zIndex: (theme) => theme.zIndex.appBar + 100}}
-				variant="temporary"
+				variant="permanent"
 			>
 				{content}
 			</Drawer>
 		);
-	};
+	}
+
+	return (
+		<Drawer
+			anchor="left"
+			onClose={onClose}
+			open={open}
+			PaperProps={{
+				sx: {
+					backgroundColor: "neutral.900",
+					color: "#FFFFFF",
+					width: 280,
+				},
+			}}
+			sx={{ zIndex: (theme) => theme.zIndex.appBar + 100 }}
+			variant="temporary"
+		>
+			{content}
+		</Drawer>
+	);
+};
 
 DashboardSidebar.propTypes = {
-  onClose: PropTypes.func,
-  open: PropTypes.bool
+	onClose: PropTypes.func,
+	open: PropTypes.bool
 };
