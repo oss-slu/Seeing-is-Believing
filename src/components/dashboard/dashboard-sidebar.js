92eb804c51546794c0506f4c68ea1a0e6bd8b379
sivaprasadvishnu18 <47301401+sivaprasadvishnu18@users.noreply.github.com>
2022-10-19 15:13:38 -0500

    Update sidebar

--- src/components/dashboard/dashboard-sidebar.js
+++ src/components/dashboard/dashboard-sidebar.js
@@ -19,6 +19,7 @@ const getSectionsStudent = (t) => [
 	{
 		title: t("General"),
 		items: [
+
 			{
 				title: t("Home"),
 				path: "/student",
@@ -91,11 +92,23 @@ const getSectionsTeacher = (t) => [
 const getSectionsAdministrator = (t) => [
 	{
 		title: t("General"),
-		items: [
+		/*items: [
 			{
 				title: t("General"),
+				items: [
+					{
+						title: t("Home"),
+						path: "/administrator",
+						icon: <HomeIcon fontSize="small" />,
+					},
+					{
+						title: t("Add a Langage"),
+						path: "/administrator/language",
+						icon: <LanguageIcon fontSize="small" />,
+					},
+				]
 			}
-		]
+		]*/
 	}
 
 ];
@@ -111,13 +124,13 @@ export const DashboardSidebar = (props) => {
 	//const sections = useMemo(() => user.status === "Student" ? getSectionsStudent(t): getSectionsTeacher(t), [t]);
 	const sections = useMemo(() => {
 		if (user.status === "student") {
-			getSectionsStudent(t)
-		} else if (user.staus === "teacher") {
-			getSectionsTeacher(t);
+			return getSectionsStudent(t)
 		} else if (user.status === "administrator") {
-			getSectionsAdministrator(t);
+			return getSectionsAdministrator(t);
+		} else {
+			return getSectionsTeacher(t);
 		}
-	});
+	}, [t]);
 
 	const handleLogout = async () => {
 		try {
