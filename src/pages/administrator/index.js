df9c5231318b4025ca92d3ed48767b0ce738127c
sivaprasadvishnu18 <47301401+sivaprasadvishnu18@users.noreply.github.com>
2022-10-26 21:22:11 -0500

    Create admin folder in pages

--- src/pages/administrator/index.js
+++ src/pages/administrator/index.js
@@ -55,10 +55,10 @@ const Overview = () => {
 		}
   },[fetchedClasses])
   
-	useEffect(() => {
+	/*useEffect(() => {
 		gtm.push({event: "page_view"});
 		fetchDataClasses()
-	}, []);*/
+	}, []);
 
 	return (
 		<Box>
