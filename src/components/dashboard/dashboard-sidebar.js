46a24c12307e4023361f365330316b87903f76ae
danmao1 <daniel.mao@slu.edu>
2022-11-18 12:32:44 -0600

    deleted duplicated imports

--- src/components/dashboard/dashboard-sidebar.js
+++ src/components/dashboard/dashboard-sidebar.js
@@ -9,18 +9,13 @@ import { Home as HomeIcon } from '../../icons/home';
 import { DocumentText as DocumentIcon } from '../../icons/document-text';
 import { Scrollbar } from '../scrollbar';
 import { DashboardSidebarSection } from './dashboard-sidebar-section';
-<<<<<<< HEAD
 import {PencilAlt as PenIcon} from '../../icons/pencil-alt'
 import {ViewList as GradesIcon} from '../../icons/view-list';
 import { InstructionsBook as InstructionsIcon } from '../../icons/instruction-book';
-=======
-import { PencilAlt as PenIcon } from '../../icons/pencil-alt'
-import { ViewList as GradesIcon } from '../../icons/view-list';
->>>>>>> 383a5e3 (Added getSectionsAdministrator)
+
 import { Language as LanguageIcon } from '../../icons/language';
 import { Save as SaveIcon } from '../../icons/save';
 import { DotsHorizontal as DotsHorizontalIcon } from '../../icons/dots-horizontal'
-import { InstructionsBook as InstructionsIcon } from '../../icons/instruction-book';
 
 const getSectionsStudent = (t) => [
 	{
