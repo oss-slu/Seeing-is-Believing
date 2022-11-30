c232769a7e7951381a0a9895e2787ced325e6081
danmao1 <daniel.mao@slu.edu>
2022-11-30 15:02:48 -0600

    Fixed Word's Library to Word Library

--- src/components/teacher/OverviewHomeworks.js
+++ src/components/teacher/OverviewHomeworks.js
@@ -66,18 +66,6 @@ const Content = (props) => {
 	const seeHomework = (homework_id) => {
 		router.push(`/teacher/homeworks/${homework_id}`);
 	};
-	async function deleteHomework(homework_id){
-		await firebase
-			.firestore()
-			.collection('assignments')
-			.doc(homework_id)
-			.delete()
-			.catch((e) => console.log(e));
-		window.location.reload(false);
-
-
-	};
-	
 	if (isLoaded) {
 		return (
 			<>
