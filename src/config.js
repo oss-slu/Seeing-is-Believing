3237eb09300b5dd789b1f733546af4989418e537
OwenAThomasson <owenathomasson@gmail.com>
2023-10-02 13:47:17 -0500

    Fixes issue 57

--- src/config.js
+++ src/config.js
@@ -13,13 +13,13 @@ export const auth0Config = {
 
 export const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "seeing-is-beleiving.firebaseapp.com",
-  databaseURL: "https://seeing-is-beleiving-default-rtdb.firebaseio.com",
-  projectId: "seeing-is-beleiving",
-  storageBucket: "seeing-is-beleiving.appspot.com",
-  messagingSenderId: "955481005378",
-  appId: "1:955481005378:web:d5145ada8733d1804d7cdb",
-  measurementId: "G-41PZRQMEBP"
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 /*export const gtmConfig = {
