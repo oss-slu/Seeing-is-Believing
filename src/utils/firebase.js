f15b0b7d0d145f4d5786d41969340e2cd5dfb4cf
akidSLU <anna.kidwell@slu.edu>
2023-10-01 19:51:40 -0500

    removed hard coded values from config.js and stored in .env.local or .env.production which are ignored in .getignote

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,11 +1,8 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
-}
+// import the configured Firebase instance from config.js
+import firebaseConfig from '../config';
 
 //If an firebase app hasn't already been created
 if (!firebase.apps.length) {
