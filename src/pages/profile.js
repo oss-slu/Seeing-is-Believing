28cf727e33b4c3900bf26113c4645c59a12d312b
akidSLU <anna.kidwell@slu.edu>
2024-02-11 01:00:05 -0600

    finished UI design

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -3,42 +3,77 @@ import {
 	Box,
 	Typography,
 	Button,
-	IconButton,
 	DialogTitle,
 	Dialog,
-	List,
-	ListItem,
-	ListItemButton,
 	Tooltip,
-	Chip,
 	DialogContent,
 	Grid,
-	Item,
 	Card,
 	CardContent,
 	Divider,
-	Container
+	Container,
+	TextField,
+	Select,
+	MenuItem,
 } from "@mui/material";
 import { DashboardLayout } from "../components/dashboard/dashboard-layout";
 import { useAuth } from "../hooks/use-auth";
-import React, { useState } from "react";
-import EditIcon from '@mui/icons-material/Edit';
+import React, { useEffect, useState } from "react";
 import Stack from "@mui/material/Stack";
 import { AuthGuard } from "../components/authentication/auth-guard";
+import {db} from "../lib/firebase"
 
 
 const Profile = () => {
 
 	const {user,logout } = useAuth();
+	
 	const pronouns = ['She/Her/Hers', 'He/Him/His', 'They/Them/Theirs'];
-	const [pronounOpen, setPronounOpen] = useState(false);
-	const [selectedPronoun, setSelectedPronoun] = useState(pronouns[1]);
-	// TODO: pull pronoun from database
+	const [selectedPronoun, setSelectedPronoun] = useState('');
+
+	const [biography, setBiography] = useState('');
+
+	const [languages, setLanguages] = useState([]);
+	const [selectedLanguage, setSelectedLanguage] = useState("");
 
 	const [editOpen, setEditOpen] = useState(false);
 
-
 	const [pictureEntryOpen, setPictureEntryOpen] = useState(false);
+	
+	// TODO: pull picture from database
+	const userPicture = "/static/images/placeholder.png"
+
+	const fetchDataLanguages = async () => {
+		const collection = await db.collection("languages");
+		let results = [];
+		await collection.get().then((snapshot) => {
+			snapshot.docs.forEach((doc) => {
+				const newLanguage = doc.data();
+				newLanguage={id:doc.id,...newLanguage}
+				results.push(newLanguage);
+			});
+		});
+		setLanguages(results);
+	};
+
+	useEffect(() => {
+		fetchDataLanguages();
+	}, []);
+
+	const firstToUpperCase=(i)=>{
+		if(i){
+		  return i.charAt(0).toUpperCase()
+		}
+		return ""
+	}
+
+	const handlePictureEntryOpen = () => {
+		setPictureEntryOpen(true);
+	}
+
+	const handlePictureEntryClose = () => {
+		setPictureEntryOpen(false);
+	}
 
 	const handleEditOpen = () => {
 		setEditOpen(true);
@@ -47,37 +82,10 @@ const Profile = () => {
 	const handleEditClose = () => {
 		setEditOpen(false);
 	};
-	
-	// TODO: pull picture from database
-	const userPicture = "/static/images/placeholder.png"
 
-	const firstToUpperCase=(i)=>{
-		if(i){
-		  return i.charAt(0).toUpperCase()
-		}
-		return ""
-	  }
-
-	const handlePronounOpen = () => {
-		setPronounOpen(true);
-	};
-
-	const handlePronounSelected = (value) => {
-		setPronounOpen(false);
-		setSelectedPronoun(value);
-		// TODO: save to firebase database
-	};
-
-	const handlePronounClose = () => {
-		setPronounOpen(false);
-	};
-
-	const handlePictureEntryOpen = () => {
-		setPictureEntryOpen(true);
-	}
-
-	const handlePictureEntryClose = () => {
-		setPictureEntryOpen(false);
+	const handleProfileUpdate = () => {
+		// TODO: logic for updating Firebase profile fields
+		handleEditClose();
 	}
 
 	return(
@@ -93,146 +101,203 @@ const Profile = () => {
 			>
 				<Container maxWidth="xl">
 					
-					{/* <Grid item>
-						<Typography variant="h4">Homework</Typography>
-					</Grid>
-
-					<Grid item mt={2} xs={10}>
-						<Typography variant="subtitle1" sx={{my: 1}}>
-							Select the homework you want to see the grades
-						</Typography>
-					</Grid> */}
-					
 					<Grid container>
-						<Stack
-							direction="row"
-							justifyContent="flex-start"
-							alignItems="flex-end"
-							spacing={12}
-							>
-							<Typography variant="h2" mt={2}>
-								Profile
-							</Typography>
-						
-							<Button variant="outlined" onClick={() => handleEditOpen()}>
-								Edit Profile
-							</Button>
-						</Stack>
 
+						{/* Edit Profile Dialog */}
 						<Dialog onClose={handleEditClose} open={editOpen}>
 							<DialogTitle>
 								Edit Your Profile
 							</DialogTitle>
 
 							<DialogContent>
-								<Card 
-									variant="outlined" 
-									sx={{ 
-										width: '100%',
-									}}>
 
-									<CardContent>
-										<Stack
-											direction="row"
-											spacing={12}
-										>
-											<Typography variant="h5">
-												Email
-											</Typography>
+								{/* Biography */}
+								<Stack
+									direction="row"
+									spacing={12}
+									mb={2}
+									mt={2}
+								>
+									<Typography variant="h6" mt={1}>
+										Biography
+									</Typography>
 
-											<Typography variant="h5">
-												{user.email}
-											</Typography>
-										</Stack>
-									</CardContent>
-								</Card>
+									<TextField
+										fullWidth
+										sx={{mb: 2, mt: 1}}
+										id="outlined-multiline-flexible"
+										label="Enter Biography"
+										multiline
+										maxRows={4}
+										value={biography}
+										onChange={(event) => setBiography(event.target.value)}
+									/>
+								</Stack>
+
+								<Divider/>
+
+								{/* Pronouns */}
+								<Stack
+									direction="row"
+									spacing={12}
+									mb={2}
+									mt={2}
+								>
+									<Typography variant="h6" mt={1}>
+										Pronouns
+									</Typography>
+
+									<Select
+										displayEmpty
+										fullWidth
+										sx={{mb: 2, mt: 1}}
+										labelId="pronounSelect"
+										id="pronounSelect"
+										value={selectedPronoun}
+										label="Pronoun"
+										onChange={(event) => setSelectedPronoun(event.target.value)}
+									>
+										{pronouns.map((pronoun) => (
+											<MenuItem key = {pronoun} value={pronoun}>
+												{pronoun}
+											</MenuItem>
+										))}
+
+									</Select>
+
+								</Stack>
+
+								<Divider/>
+
+								{/* Language */}
+								<Stack
+									direction="row"
+									spacing={12}
+									mb={2}
+									mt={2}
+								>
+									<Typography variant="h6" mt={1}>
+										Language
+									</Typography>
+
+									<Select
+										displayEmpty
+										fullWidth
+										sx={{mb: 2, mt: 1}}
+										labelId="langaugeSelect"
+										id="languageSelect"
+										value={selectedLanguage}
+										label="language"
+										onChange={(event) => setSelectedLanguage(event.target.value)}
+									>
+
+									{languages.map((language, pos) => {
+										return (
+											<MenuItem key={pos} value={language.name}>
+												{language.name}
+											</MenuItem>
+										);
+									})}
+
+									</Select>
+
+								</Stack>
+
 							</DialogContent>
 
-							<List>
-								{pronouns.map((pronoun) => (
-									<ListItem key={pronoun}>
-										<ListItemButton onClick={() => handlePronounSelected(pronoun)}>
-											{pronoun}
-										</ListItemButton>
-									</ListItem>
-								))}
-							</List>
+							<Button onClick={() => handleProfileUpdate()}>
+								Save
+							</Button>
 						</Dialog>
 
 					</Grid>
 
-					<Grid container mt={2}>
-						<Grid item>
-							<Tooltip
-								title="Edit Profile Picture">
-									<div onClick={() => handlePictureEntryOpen()}>
-										<Avatar
-											alt={user.firstName}
-											src={userPicture}
-											sx={{
-												bgcolor:'#7582EB',
-												height: 80,
-												width: 80
-											}}
-										>
-											{user && (firstToUpperCase(user.firstName)+ firstToUpperCase(user.lastName))}
-										</Avatar>
-									</div>
-							</Tooltip>
-						</Grid>
-						
-						<Box
-							sx={{
-								ml: 2,
-								mt: 1
-							}}
-						>
-							{/* Name */}
-							<Typography 
-								variant="h4">
-								{user &&(user.firstName +" "+ user.lastName)  }
-							</Typography>
-
-							{/* Pronouns */}
-							<Typography
-								color="textSecondary"
-								variant="body2"
+					{/* Profile Picture + Name + Pronouns + Edit Profile Button */}
+					<Stack
+							direction="row"
+							justifyContent="flex-start"
+							alignItems="flex-end"
+							spacing={-20}
 							>
-								{selectedPronoun}
-							</Typography>
-								
-							{/* Upload Picture Dialog */}
-							<Dialog onClose={handlePictureEntryClose} open={pictureEntryOpen}>
-								<DialogTitle>
-									Select Profile Picture
-								</DialogTitle>
-								<DialogContent>
-									Upload a Picture
-									{/* TODO: logic for allowing user to upload their own profile picture */}
+							
+							<Grid container mt={2}>
+									<Grid item>
+										<Tooltip
+											title="Edit Profile Picture">
+												<div onClick={() => handlePictureEntryOpen()}>
+													<Avatar
+														alt={user.firstName}
+														src={userPicture}
+														sx={{
+															bgcolor:'#7582EB',
+															height: 80,
+															width: 80
+														}}
+													>
+														{user && (firstToUpperCase(user.firstName)+ firstToUpperCase(user.lastName))}
+													</Avatar>
+												</div>
+										</Tooltip>
+									</Grid>
 									
-								</DialogContent>
-							</Dialog>
-					</Box>
+									<Box
+										sx={{
+											ml: 2,
+											mt: 1
+										}}
+									>
+										{/* Name */}
+										<Typography 
+											variant="h4">
+											{user &&(user.firstName +" "+ user.lastName)  }
+										</Typography>
 
-				</Grid>
+										{/* Pronouns */}
+										<Typography
+											color="textSecondary"
+											variant="body2"
+										>
+											{selectedPronoun}
+										</Typography>
+											
+										{/* Upload Picture Dialog */}
+										<Dialog onClose={handlePictureEntryClose} open={pictureEntryOpen}>
+											<DialogTitle>
+												Select Profile Picture
+											</DialogTitle>
+											<DialogContent>
+												Upload a Picture
+												{/* TODO: logic for allowing user to upload their own profile picture */}
+												
+											</DialogContent>
+										</Dialog>
+								</Box>
+
+							</Grid>
+						
+							<Button variant="outlined" onClick={() => handleEditOpen()}>
+								Edit Profile
+							</Button>
+						</Stack>
 
 				<Grid container mt={4}>
 					<Card 
 						variant="outlined" 
 						sx={{ 
-							width: '100%',
+							width: '80%',
+							height: '50%'
 						}}>
 
 						<CardContent>
 							<Stack
 								direction="row"
-								spacing={45}
+								spacing={44}
 							>
-								<Typography variant="h5">
+								<Typography variant="h6">
 									Email
 								</Typography>
 
-								<Typography variant="h5">
+								<Typography variant="h6">
 									{user.email}
 								</Typography>
 							</Stack>
@@ -242,7 +307,7 @@ const Profile = () => {
 					<Card 
 						variant="outlined" 
 						sx={{ 
-							width: '100%',
+							width: '80%',
 						}}>
 
 						<CardContent>
@@ -250,11 +315,11 @@ const Profile = () => {
 								direction="row"
 								spacing={35}
 							>
-								<Typography variant="h5">
+								<Typography variant="h6">
 									Organization
 								</Typography>
 
-								<Typography variant="h5">
+								<Typography variant="h6">
 									{user.organization}
 								</Typography>
 							</Stack>
@@ -264,7 +329,7 @@ const Profile = () => {
 					<Card 
 						variant="outlined" 
 						sx={{ 
-							width: '100%',
+							width: '80%',
 						}}>
 
 						<CardContent>
@@ -272,11 +337,11 @@ const Profile = () => {
 								direction="row"
 								spacing={34}
 							>
-								<Typography variant="h5">
+								<Typography variant="h6">
 									Account Type
 								</Typography>
 
-								<Typography variant="h5">
+								<Typography variant="h6">
 									{user.status}
 								</Typography>
 							</Stack>
@@ -286,7 +351,7 @@ const Profile = () => {
 					<Card 
 						variant="outlined" 
 						sx={{ 
-							width: '100%',
+							width: '80%',
 						}}>
 
 						<CardContent>
@@ -294,12 +359,12 @@ const Profile = () => {
 								direction="row"
 								spacing={39}
 							>
-								<Typography variant="h5">
+								<Typography variant="h6">
 									Biography
 								</Typography>
 
-								<Typography variant="h5">
-									TBD
+								<Typography variant="body1">
+									{biography}
 								</Typography>
 							</Stack>
 						</CardContent>
@@ -308,7 +373,7 @@ const Profile = () => {
 					<Card 
 						variant="outlined" 
 						sx={{ 
-							width: '100%',
+							width: '80%',
 						}}>
 
 						<CardContent>
@@ -316,12 +381,12 @@ const Profile = () => {
 								direction="row"
 								spacing={39}
 							>
-								<Typography variant="h5">
+								<Typography variant="h6">
 									Language
 								</Typography>
 
-								<Typography variant="h5">
-									TBD
+								<Typography variant="h6">
+									{selectedLanguage}
 								</Typography>
 							</Stack>
 						</CardContent>
@@ -330,140 +395,6 @@ const Profile = () => {
 				</Grid>
 				
 				</Container>
-			</Box>
-
-
-
-
-{/* bad stuff below */}
-			<Box
-				sx={{
-					// display: 'flex',
-					// flex: '1 1 auto',
-					// alignItems:'left',
-					// flexDirection: 'column',
-					// width: '100%',
-					flexGrow: 1,
-					pt: 5,
-					pb: 8,
-				}}
-			>
-
-				<Stack
-					direction="row"
-					justifyContent="space-evenly"
-					alignItems="flex-end"
-					spacing={12}
-					>
-					<Typography variant="h2" mt={2}>
-						Profile
-					</Typography>
-					<Button variant="outlined">
-						Edit Profile
-					</Button>
-				</Stack>
-				
-				<Box
-					sx={{
-					alignItems: 'center',
-					p: 2,
-					display: 'flex',
-					marginLeft: '24%',
-					flexGrow: 1,
-					pt: 5,
-					pb: 8,
-					}}
-				>
-					{/* TODO: insert image src */}
-					<Tooltip
-						title="Edit Profile Picture">
-							<div onClick={() => handlePictureEntryOpen()}>
-								<Avatar
-									alt={user.firstName}
-									src={userPicture}
-									sx={{
-										bgcolor:'#7582EB',
-										height: 80,
-										width: 80
-									}}
-								>
-									{user && (firstToUpperCase(user.firstName)+ firstToUpperCase(user.lastName))}
-								</Avatar>
-							</div>
-					</Tooltip>
-					<Box
-						sx={{
-							ml: 2
-						}}
-					>
-
-						{/* Name */}
-						<Typography 
-							variant="h4">
-							{user &&(user.firstName +" "+ user.lastName)  }
-						</Typography>
-						
-						<Stack direction="row" spacing={2}>
-						
-						{/* Pronouns */}
-						<Typography
-							color="textSecondary"
-							variant="body2"
-						>
-							{selectedPronoun}
-						</Typography>
-						<IconButton
-						onClick={() => handlePronounOpen()}
-							sx={{
-								height: 20,
-								width: 20
-							}}>
-							<EditIcon sx={{
-								height: 20,
-								width: 20
-							}}>
-
-							</EditIcon>
-						</IconButton>
-						
-						<Dialog onClose={handlePronounClose} open={pronounOpen}>
-							<DialogTitle>
-								Set Your Pronouns
-							</DialogTitle>
-							<List>
-								{pronouns.map((pronoun) => (
-									<ListItem key={pronoun}>
-										<ListItemButton onClick={() => handlePronounSelected(pronoun)}>
-											{pronoun}
-										</ListItemButton>
-									</ListItem>
-								))}
-							</List>
-						</Dialog>
-						
-						</Stack>
-
-						{/* Upload Picture Dialog */}
-						<Dialog onClose={handlePictureEntryClose} open={pictureEntryOpen}>
-							<DialogTitle>
-								Select Profile Picture
-							</DialogTitle>
-							<DialogContent>
-								Upload a Picture
-								{/* TODO: logic for allowing user to upload their own profile picture */}
-								
-							</DialogContent>
-						</Dialog>
-					
-						{/* <Typography
-							color="textSecondary"
-							variant="body2"
-						>
-							{user && (user.organization)}
-						</Typography> */}
-					</Box>
-				</Box>
-
 			</Box>
 		</>
 	);
